eed9c95f40b358c4e8f3e5f70efe06814ed14c48
Zwazel <zwazel@hotmail.com>
2023-05-09 14:52:39 +0200

    init spring

---
+++ src/main/java/dev/groupb/m306groupb/M306GroupBApplication.java
@@ -0,0 +1,13 @@
+package dev.groupb.m306groupb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M306GroupBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M306GroupBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/groupb/m306groupb/M306GroupBApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.groupb.m306groupb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M306GroupBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

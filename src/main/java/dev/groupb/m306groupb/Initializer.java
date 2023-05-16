207f983256c6f0fdff9b38bf8e123a6ea930fa07
xXZAWARUDOXx <vergilin@bzz.ch>
2023-05-16 16:43:22 +0200

    #1  + ESLCache.java  + ESLFile.java  + ESLFileReader.java
    modified: Initializer.java --> edited for ESL Files

--- src/main/java/dev/groupb/m306groupb/Initializer.java
+++ src/main/java/dev/groupb/m306groupb/Initializer.java
@@ -1,8 +1,11 @@
 package dev.groupb.m306groupb;
 
+import dev.groupb.m306groupb.model.ESLFile.ESLCache;
+import dev.groupb.m306groupb.model.ESLFile.ESLFile;
 import dev.groupb.m306groupb.model.FileDate;
 import dev.groupb.m306groupb.model.SDATFile.SDATCache;
 import dev.groupb.m306groupb.model.SDATFile.SDATFile;
+import dev.groupb.m306groupb.utils.ESLFileReader;
 import dev.groupb.m306groupb.utils.FileReader;
 import dev.groupb.m306groupb.utils.SDATFileReader;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,21 +24,40 @@ public class Initializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        // SDAT Files init
         SDATCache sdatCache = SDATCache.getInstance();
 
         SDATFileReader sdatFileReader = new SDATFileReader();
-        File[] files = FileReader.getFiles(sdat_files_path);
+        File[] SDATFiles = FileReader.getFiles(sdat_files_path);
 
-        int amountFilesToLoad = files.length;
-        int amountFilesLoaded = 0;
-        for (File file : files) {
+        int SDATamountFilesToLoad = SDATFiles.length;
+        int SDATamountFilesLoaded = 0;
+        for (File file : SDATFiles) {
             SDATFile sdatFile = sdatFileReader.parseFile(file);
             FileDate fileDate = sdatFileReader.getFileDate(file);
 
             sdatCache.addSDATFile(fileDate, sdatFile);
 
-            amountFilesLoaded++;
-            System.out.println("Loading SDAT files: " + amountFilesLoaded + "/" + amountFilesToLoad + " (" + (int) ((double) amountFilesLoaded / amountFilesToLoad * 100) + "%)");
+            SDATamountFilesLoaded++;
+            System.out.println("Loading SDAT files: " + SDATamountFilesLoaded + "/" + SDATamountFilesToLoad + " (" + (int) ((double) SDATamountFilesLoaded / SDATamountFilesToLoad * 100) + "%)");
+        }
+
+        // ESL Files init
+        ESLCache eslCache = ESLCache.getInstance();
+
+        ESLFileReader eslFileReader = new ESLFileReader();
+        File[] ESLFiles = FileReader.getFiles(esl_files_path);
+
+        int ESLamountFilesToLoad = ESLFiles.length;
+        int ESLamountFilesLoaded = 0;
+        for (File file : ESLFiles) {
+            ESLFile eslFile = eslFileReader.parseFile(file);
+            FileDate fileDate = eslFileReader.getFileDate(file);
+
+            eslCache.addESLFile(fileDate, eslFile);
+
+            ESLamountFilesLoaded++;
+            System.out.println("Loading ESL files: " + ESLamountFilesLoaded + "/" + ESLamountFilesToLoad + " (" + (int) ((double) ESLamountFilesLoaded / ESLamountFilesToLoad * 100) + "%)");
         }
     }
 }

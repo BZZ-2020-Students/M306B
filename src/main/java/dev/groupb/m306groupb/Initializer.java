57897108f5f334d437d196d5e73c4eb6c2dd3a65
Zwazel <zwazel@hotmail.com>
2023-05-16 13:55:07 +0200

    added filter for file change listener

--- src/main/java/dev/groupb/m306groupb/Initializer.java
+++ src/main/java/dev/groupb/m306groupb/Initializer.java
@@ -10,7 +10,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.util.HashMap;
 
 @Component
 public class Initializer implements CommandLineRunner {

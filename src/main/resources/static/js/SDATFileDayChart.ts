427e28379860296ecbfb5fe1d22555f183d66f73
Tim Irmler <zwazel@hotmail.com>
2023-05-28 17:26:10 +0200

    wip updating frontend

--- src/main/resources/static/js/SDATFileDayChart.ts
+++ src/main/resources/static/js/SDATFileDayChart.ts
@@ -35,31 +35,71 @@ interface SdatFile {
     observations: Observation[];
 }
 
-interface Data {
+interface SdatWithFileDate {
     fileDate: FileDate;
-    sdatfiles: SdatFile[];
+    sdatFiles: SdatFile[];
 }
 
-async function SDATFileDayChart(sdatFilesRaw: any) {
-    let jsonData: Data = JSON.parse(sdatFilesRaw);
+function SDATFileDayChart(sdatFilesRaw: any) {
+    let jsonData: SdatWithFileDate[] = JSON.parse(sdatFilesRaw);
+    console.log(jsonData)
 
-    const datasets = jsonData.sdatfiles.map(sdatFile => {
-        return {
-            label: sdatFile.fileType,
-            data: sdatFile.observations.map(observation => observation.volume)
+    if (!Array.isArray(jsonData)) {
+        console.error('jsonData is not an array');
+        return;
+    }
+
+    const fileTypes: FileType[] = [];
+    for (let i = 0; i < jsonData.length; i++) {
+        const sdatWithFileDate = jsonData[i];
+        for (let j = 0; j < sdatWithFileDate.sdatFiles.length; j++) {
+            const sdatFile = sdatWithFileDate.sdatFiles[j];
+            if (fileTypes.indexOf(sdatFile.fileType) === -1) {
+                fileTypes.push(sdatFile.fileType);
+            }
         }
-    });
+    }
 
-    const startDate = jsonData.fileDate.startDate
-    const resolution = jsonData.sdatfiles[0].resolution;
-    let dates = [];
-    jsonData.sdatfiles[0].observations.forEach(function (observation) {
-        let minute = (observation.position - 1) * resolution.resolution;
-        let newDate = new Date(startDate)
-        newDate.setMinutes(newDate.getMinutes() + minute)
+    const datasets = [];
+    for (let i = 0; i < fileTypes.length; i++) {
+        const fileType = fileTypes[i];
+        const data: number[] = [];
+        for (let j = 0; j < jsonData.length; j++) {
+            const sdatWithFileDate = jsonData[j];
+            for (let k = 0; k < sdatWithFileDate.sdatFiles.length; k++) {
+                const sdatFile = sdatWithFileDate.sdatFiles[k];
+                if (sdatFile.fileType === fileType) {
+                    for (let l = 0; l < sdatFile.observations.length; l++) {
+                        const observation = sdatFile.observations[l];
+                        data.push(observation.volume);
+                    }
+                }
+            }
+        }
+        datasets.push({
+            label: fileType,
+            data: data
+        });
+    }
 
-        dates.push(newDate.toLocaleString())
-    })
+    const startDate = jsonData[0].fileDate.startDate
+    const resolution = jsonData[0].sdatFiles[0].resolution;
+    let dates: string[] = [];
+    for (let i = 0; i < jsonData.length; i++) {
+        const sdatWithFileDate = jsonData[i];
+        for (let j = 0; j < sdatWithFileDate.sdatFiles.length; j++) {
+            const sdatFile = sdatWithFileDate.sdatFiles[j];
+            for (let k = 0; k < sdatFile.observations.length; k++) {
+                const observation = sdatFile.observations[k];
+                let minute = (observation.position - 1) * resolution.resolution;
+                let newDate = new Date(startDate)
+                newDate.setMinutes(newDate.getMinutes() + minute)
+
+                dates.push(newDate.toLocaleString())
+            }
+        }
+    }
+
     const data = {
         labels: dates,
         datasets: datasets
@@ -74,3 +114,4 @@ async function SDATFileDayChart(sdatFilesRaw: any) {
         }
     );
 }
+

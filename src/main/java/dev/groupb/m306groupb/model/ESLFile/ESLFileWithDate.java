5a8148c4416d66551c982883b5173438fe0d9fca
xXZAWARUDOXx <vergilin@bzz.ch>
2023-05-30 15:30:57 +0200

    #3  + finished ESL-File Reading
    modified: sorting ESL Files

---
+++ src/main/java/dev/groupb/m306groupb/model/ESLFile/ESLFileWithDate.java
@@ -0,0 +1,17 @@
+package dev.groupb.m306groupb.model.ESLFile;
+
+import dev.groupb.m306groupb.model.FileDate;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ESLFileWithDate implements Comparable<ESLFileWithDate>{
+    private ESLFile[] ESLFiles;
+    private FileDate fileDate;
+
+    @Override
+    public int compareTo(ESLFileWithDate o) {
+        return this.fileDate.compareTo(o.fileDate);
+    }
+}
--- src/main/java/dev/groupb/m306groupb/model/meterReading/Calculator.java
+++ src/main/java/dev/groupb/m306groupb/model/meterReading/Calculator.java
@@ -1,20 +1,38 @@
 package dev.groupb.m306groupb.model.meterReading;
 
 import dev.groupb.m306groupb.enums.EconomicActivity;
+import dev.groupb.m306groupb.model.ESLFile.ESLCache;
+import dev.groupb.m306groupb.model.ESLFile.ESLFile;
+import dev.groupb.m306groupb.model.ESLFile.ESLFileWithDate;
+import dev.groupb.m306groupb.model.FileDate;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Calculator {
     private LocalDate date;
     private double value;
     private EconomicActivity economicActivity;
 
-    public Calculator() {
+    ESLCache eslCache;
 
+    public Calculator() {
+        eslCache = ESLCache.getInstance();
     }
 
-    public void calculateValue() {
-
+    public void sortValues() {
+        HashMap<FileDate, ESLFile[]> eslFileHashMap = (HashMap<FileDate, ESLFile[]>) eslCache.getEslFileMap();
+        // sort by start date
+        List<ESLFileWithDate> fileDateESLFilesList = new ArrayList<>(eslFileHashMap.entrySet().stream()
+                .map(fileDateEntry -> ESLFileWithDate
+                    .builder()
+                    .fileDate(fileDateEntry.getKey())
+                    .ESLFiles(fileDateEntry.getValue())
+                    .build())
+                    .toList());
+        fileDateESLFilesList.sort(ESLFileWithDate::compareTo);
     }
 
     public LocalDate getDate() {

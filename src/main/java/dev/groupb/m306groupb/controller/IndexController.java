772b16c47c822ff251975830666312215df7a46e
Tim Irmler <zwazel@hotmail.com>
2023-06-02 11:19:50 +0200

    getting same day data

--- src/main/java/dev/groupb/m306groupb/controller/IndexController.java
+++ src/main/java/dev/groupb/m306groupb/controller/IndexController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -35,13 +36,13 @@ public class IndexController {
 
                 try {
                     ConcurrentHashMap<FileDate, SDATFile[]> sdatFileHashMap = sdatCache.getSdatFileHashMap();
-                    // sort by start date
+// sort by start date
                     List<SDATFileWithDate> fileDateSdatFilesList = new java.util.ArrayList<>(sdatFileHashMap.entrySet().stream()
                             .map(entry -> SDATFileWithDate.builder().fileDate(entry.getKey()).SDATFiles(entry.getValue()).build())
                             .toList());
                     fileDateSdatFilesList.sort(SDATFileWithDate::compareTo);
 
-                    // get the earliest date and the latest date if they aren't specified in the request
+// get the earliest date and the latest date if they aren't specified in the request
                     Date earliestDate = (from == null) ? fileDateSdatFilesList.get(0).getFileDate().getStartDate() : from;
                     Date latestDate = (to == null) ? fileDateSdatFilesList.get(fileDateSdatFilesList.size() - 1).getFileDate().getStartDate() : to;
 
@@ -49,10 +50,33 @@ public class IndexController {
                     model.addAttribute("earliestDate", frontendDayFormat.format(earliestDate));
                     model.addAttribute("latestDate", frontendDayFormat.format(latestDate));
 
-                    // filter the list
-                    fileDateSdatFilesList = new java.util.ArrayList<>(fileDateSdatFilesList.stream()
-                            .filter(sdatFileWithDate -> !sdatFileWithDate.getFileDate().getStartDate().before(earliestDate) && !sdatFileWithDate.getFileDate().getStartDate().after(latestDate))
-                            .toList());
+                    Calendar cal1 = Calendar.getInstance();
+                    Calendar cal2 = Calendar.getInstance();
+                    cal1.setTime(earliestDate);
+                    cal2.setTime(latestDate);
+                    boolean sameDay = cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                            cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+
+                    if (sameDay) {
+                        // if from and to dates are the same day, return data for that day
+                        fileDateSdatFilesList = new java.util.ArrayList<>(fileDateSdatFilesList.stream()
+                                .filter(sdatFileWithDate -> {
+                                    Calendar cal3 = Calendar.getInstance();
+                                    cal3.setTime(sdatFileWithDate.getFileDate().getStartDate());
+                                    return cal1.get(Calendar.YEAR) == cal3.get(Calendar.YEAR) &&
+                                            cal1.get(Calendar.DAY_OF_YEAR) == cal3.get(Calendar.DAY_OF_YEAR);
+                                })
+                                .toList());
+                    } else {
+                        // filter the list
+                        Calendar cal4 = Calendar.getInstance();
+                        cal4.setTime(latestDate);
+                        cal4.add(Calendar.DATE, 1); // add one day to make the range inclusive
+                        Date inclusiveLatestDate = cal4.getTime();
+                        fileDateSdatFilesList = new java.util.ArrayList<>(fileDateSdatFilesList.stream()
+                                .filter(sdatFileWithDate -> !sdatFileWithDate.getFileDate().getStartDate().before(earliestDate) && !sdatFileWithDate.getFileDate().getStartDate().after(inclusiveLatestDate))
+                                .toList());
+                    }
 
                     SimpleDateFormat simpleDateFormat = new SimpleDateFormat(GlobalStuff.SDAT_DATE_FORMAT);
                     ObjectMapper objectMapper = new ObjectMapper();

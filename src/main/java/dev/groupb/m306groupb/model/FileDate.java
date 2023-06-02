d387cab15ecef0821bbce551c1f8ded78c752f96
Tim Irmler <zwazel@hotmail.com>
2023-06-02 10:58:31 +0200

    somewhat fixed bug

--- src/main/java/dev/groupb/m306groupb/model/FileDate.java
+++ src/main/java/dev/groupb/m306groupb/model/FileDate.java
@@ -21,4 +21,26 @@ public class FileDate implements Comparable<FileDate> {
     public int compareTo(FileDate o) {
         return this.startDate.compareTo(o.startDate);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FileDate fileDate = (FileDate) o;
+
+        if (getFileCreationDate() != null ? !getFileCreationDate().equals(fileDate.getFileCreationDate()) : fileDate.getFileCreationDate() != null)
+            return false;
+        if (getStartDate() != null ? !getStartDate().equals(fileDate.getStartDate()) : fileDate.getStartDate() != null)
+            return false;
+        return getEndDate() != null ? getEndDate().equals(fileDate.getEndDate()) : fileDate.getEndDate() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFileCreationDate() != null ? getFileCreationDate().hashCode() : 0;
+        result = 31 * result + (getStartDate() != null ? getStartDate().hashCode() : 0);
+        result = 31 * result + (getEndDate() != null ? getEndDate().hashCode() : 0);
+        return result;
+    }
 }

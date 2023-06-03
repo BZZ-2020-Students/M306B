5224449f074c426fbeefe8d34523a88accb0a237
Tim Irmler <zwazel@hotmail.com>
2023-06-03 13:17:10 +0200

    working on json export

--- src/main/java/dev/groupb/m306groupb/controller/CsvExportService.java
+++ src/main/java/dev/groupb/m306groupb/controller/CsvExportService.java
@@ -1,5 +1,7 @@
 package dev.groupb.m306groupb.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.groupb.m306groupb.enums.ExportTypes;
 import dev.groupb.m306groupb.model.FileDate;
 import dev.groupb.m306groupb.model.SDATFile.SDATCache;
 import dev.groupb.m306groupb.model.SDATFile.SDATFile;
@@ -29,17 +31,20 @@ public class CsvExportService {
     private final SDATCache cacheData = SDATCache.getInstance();
 
     /**
-     * Exports data as a CSV file within a specific time range.
+     * Exports the data to a specified format
      *
-     * @param from     Start date of the time range (in yyyy-MM-dd format)
-     * @param to       End date of the time range (in yyyy-MM-dd format)
-     * @param response HttpServletResponse to set the CSV file as the response
+     * @param typePath The type of the export (csv, json)
+     * @param from     The start date of the export
+     * @param to       The end date of the export
+     * @param response The response object
+     * @throws IOException If the export fails
      */
     @GetMapping(
-            value = "/csv/{from}/{to}",
+            value = "/{type}/{from}/{to}",
             produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
     )
     public void exportDataCSV(
+            @PathVariable("type") String typePath,
             @PathVariable("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date from,
             @PathVariable("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date to,
             HttpServletResponse response
@@ -74,6 +79,13 @@ public class CsvExportService {
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         }
 
+        switch (ExportTypes.fromString(typePath)) {
+            case CSV -> exportDataCSV(response, filteredMap, from, to);
+            case JSON -> exportDataJSON(response, filteredMap, from, to);
+        }
+    }
+
+    private void exportDataCSV(HttpServletResponse response, Map<FileDate, SDATFile[]> filteredMap, Date from, Date to) throws IOException {
         response.setContentType("text/csv");
         SimpleDateFormat dateFormat = new SimpleDateFormat(GlobalStuff.FILENAME_DATE_FORMAT);
         String currentDateTime = dateFormat.format(new Date());
@@ -104,7 +116,7 @@ public class CsvExportService {
                 csvContent.put("resolutionType", sdatFile.getResolution().getTimeUnit());
                 csvContent.put("resolutionAmount", sdatFile.getResolution().getResolution());
                 csvContent.put("measureUnit", sdatFile.getMeasureUnit());
-                csvContent.put("observations", sdatFile.getObservations());
+                csvContent.put("observations", sdatFile.observationsToCSV());
                 csvWriter.write(csvContent, nameMapping);
             }
         }
@@ -112,46 +124,12 @@ public class CsvExportService {
         csvWriter.close();
     }
 
-    @GetMapping(
-            value = "/json/{from}/{to}",
-            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
-    )
-    public void exportDataJSON(
-            @PathVariable("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date from,
-            @PathVariable("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date to,
-            HttpServletResponse response
-    ) throws IOException {
-        // Retrieve the relevant SDATFiles from the SDATCache within the specified time range
-        Calendar cal1 = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
-        cal1.setTime(from);
-        cal2.setTime(to);
-        boolean sameDay = cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+    private void exportDataJSON(HttpServletResponse response, Map<FileDate, SDATFile[]> filteredMap, Date from, Date to) throws IOException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(GlobalStuff.SDAT_DATE_FORMAT);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(simpleDateFormat);
+        String json = objectMapper.writeValueAsString(filteredMap);
 
-        Map<FileDate, SDATFile[]> filteredMap;
-        if (sameDay) {
-            filteredMap = cacheData.getSdatFileHashMap().entrySet().stream().parallel()
-                    .filter(entry -> {
-                        Calendar cal3 = Calendar.getInstance();
-                        cal3.setTime(entry.getKey().getStartDate());
-                        return cal1.get(Calendar.YEAR) == cal3.get(Calendar.YEAR) &&
-                                cal1.get(Calendar.DAY_OF_YEAR) == cal3.get(Calendar.DAY_OF_YEAR);
-                    })
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        } else {
-            Calendar cal4 = Calendar.getInstance();
-            cal4.setTime(to);
-            cal4.add(Calendar.DATE, 1); // add one day to make the range inclusive
-            Date inclusiveLatestDate = cal4.getTime();
-            filteredMap = cacheData.getSdatFileHashMap().entrySet().stream().parallel()
-                    .filter(entry ->
-                            !entry.getKey().getStartDate().before(from) && !entry.getKey().getStartDate().after(inclusiveLatestDate)
-                    )
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        }
-
-        // json
         response.setContentType("application/json");
         SimpleDateFormat dateFormat = new SimpleDateFormat(GlobalStuff.FILENAME_DATE_FORMAT);
         String currentDateTime = dateFormat.format(new Date());
@@ -161,7 +139,9 @@ public class CsvExportService {
         String fromDate = dateFormat.format(from);
         String toDate = dateFormat.format(to);
         String fileName = "data_" + currentDateTime + "_from_" + fromDate + "_to_" + toDate + ".json";
+        String headerValue = "attachment; filename=" + fileName;
+        response.setHeader(headerKey, headerValue);
 
-        // todo
+        response.getWriter().write(json);
     }
 }
